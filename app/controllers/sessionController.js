bc3bceccc1bac662ae52d4889f393955835837dd
Robin <robin.plancon@outlook.com>
2023-07-17 09:55:09 +0200

    ajout du login

--- app/controllers/sessionController.js
+++ app/controllers/sessionController.js
@@ -11,19 +11,45 @@ const sessionController = {
             const { email, password } = req.body;
             // !! Votre code à partir d'ici
 
+            if (!email || !password) {
+                return res.render('login', {
+                    error: 'Veuillez remplir tous les champs',
+                });
+            }
+
             // On récupère user avec le role
+            const user = await User.findOne({
+                where: { email },
+                include: 'role',
+            });
+
             // Est-ce que l'utilisateur existe en BDD ?
             // Sélectionner user avec email et inclure le role, si on ne le trouve pas :
             //      on envoie un message d'erreur dans un objet:  {error: "Utilisateur ou mot de passe incorrect"} et on render `login` en lui passant l'erreur
             // Sinon on continue.
+            if (!user) {
+                return res.render('login', {
+                    error: 'Utilisateur ou mot de passe incorrect',
+                });
+            }
 
             // Le mot de passe est il correct ?
             // On compare le mots de passe du formulaire avec celui de l'utilisateur
             //      Si le mot de passe est incorrect : on envoie un message d'erreur dans un objet:  {error: "Utilisateur ou mot de passe incorrect"} et on render `login` en lui passant l'erreur
+            // Sinon on continue.            
+            const isGoodPassword = await bcrypt.compare(password, user.password);
+            if (!isGoodPassword) {
+                return res.render('login', {
+                    error: 'Utilisateur ou mot de passe incorrect',
+                });
+            }
 
             // On ajoute user a la session
-
+            req.session.user = user;
             // On enlève le mot de passe de la session.
+            delete req.session.user.password;
+
+            console.log(req.session.user);
 
             // !! Ne pas modifier cette ligne
             res.redirect('/');
@@ -35,6 +61,9 @@ const sessionController = {
 
     logout: (req, res) => {
         // !! Votre code ici
+        // On supprime l'utilisateur de la session
+        delete req.session.user;
+        // On redirige vers la page d'accueil
         res.redirect('/');
     },
 };
